cd0ae17365fa94128016707251f2d30eacf73708
vishalt <vishal.taksale@gmail.com>
2022-10-22 01:17:13 +0530

    Construcor _Part_1

---
+++ Constructor Part - 1/Constructor Part - 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Part___1
+{
+    class Program
+    {
+       static int a;
+        static string str;
+        static bool b;
+
+        // Default Constructor
+        public Program()
+        {
+            // Initializing the values
+            a = 1;
+            str = null;
+            b= false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(str);
+            Console.WriteLine(b);
+
+            Console.ReadLine();
+
+        }
+    }
+}
